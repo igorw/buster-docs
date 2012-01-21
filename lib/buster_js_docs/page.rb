77786caab2d7d2f1284323bf89cb54f6411b24df
August Lilleaas <august@augustl.com>
2012-01-21 20:59:39 +0100

    Using the proper HTML parser for everything.

--- lib/buster_js_docs/page.rb
+++ lib/buster_js_docs/page.rb
@@ -9,22 +9,18 @@ module BusterJsDocs
     end
 
     def parse
-      doc = Nokogiri::HTML(@template)
+      doc = Nokogiri::HTML(@template).css("body")[0]
       lines = @template.split("\n")
       header = lines[0]
 
-      if header.start_with?("<")
-        title_h1 = doc.css("h1")[0]
-        if title_h1
-          @title = title_h1.inner_text
-        else
-          @title = "Documentation"
-        end
-        @html = @template
-      else
-        @title = header
-        @html = "<h1>#{header}</h1>\n" + lines[1..-1].join("\n")
+      if doc.children[0].name != "h1"
+        h1_title = Nokogiri::XML::Node.new("h1", doc)
+        h1_title.inner_html = doc.children[0].inner_html.chomp
+        doc.children[0].replace(h1_title)
       end
+
+      @title = doc.children[0].inner_text
+      @html = doc.inner_html
     end
   end
 end
