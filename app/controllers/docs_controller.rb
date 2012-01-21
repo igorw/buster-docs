d5eff040a18a92142d8dc62c5e5353fa01a88225
August Lilleaas <august@augustl.com>
2012-01-21 20:33:10 +0100

    Displaying the title.

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -5,6 +5,8 @@ class DocsController < ApplicationController
     template = render_to_string :template => "docs/" + (params[:path] || "index"), :layout => false
     page = BusterJsDocs::Page.new(template)
     page.parse
+
+    @title = page.title
     render :text => page.html, :layout => true
   end
 end
