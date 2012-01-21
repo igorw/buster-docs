71eade875584d41a26b985d4e544fdcb8f4b2922
August Lilleaas <august@augustl.com>
2012-01-21 18:48:31 +0100

    Adding fancy header thingie

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,5 +1,16 @@
 class DocsController < ApplicationController
   def show
-    render :template => "docs/" + (params[:path] || "index")
+    html = render_to_string :template => "docs/" + (params[:path] || "index"), :layout => false
+    lines = html.split("\n")
+    header = lines[0]
+
+    if header.start_with?("<")
+      @title = "Documentation"
+    else
+      @title = header
+      html = "<h1>#{header}</h1>" + lines[1..-1].join("\n")
+    end
+
+    render :text => html, :layout => true
   end
 end
